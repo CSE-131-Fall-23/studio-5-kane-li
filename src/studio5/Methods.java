2e26640a4ff9142897351b56840fa8d68c5eb104
kevinli <kevinli@localhost>
2023-10-19 10:08:17 -0500

    Studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -17,6 +17,8 @@ public class Methods {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
+		distance = Math.sqrt(Math.pow(x2 - x1, 2)+Math.pow(y2 - y1, 2));
+		 
 		return distance;
 	}
 
@@ -34,18 +36,23 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, radius * 3.0/4.0);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
 
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, radius * 1.0/2.0);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
 
-		
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, radius * 1.0/4.0);
 	}
 
 	/**
@@ -60,7 +67,17 @@ public class Methods {
 	 *         characters in the source String with the replacement String
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
-		String result = "";
+		char [] charArray = source.toCharArray();
+		String result = "" ;
+		for (int i=0; i<source.length(); i++) {
+			if (charArray[i] == target) {
+				result = result + replacement;
+			}
+			else {
+				result = result + charArray[i];
+			}
+		}
+		
 		// TODO: Finish this method
 		
 		return result;
@@ -75,7 +92,7 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i < values.length() ; i++ )
 		return sum;
 	}
 
